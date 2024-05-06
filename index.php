a3c3fc98ccd35bd14278f22536b3c5e25b389f26
Rocky3112 <alomgirhossainrocky@gmail.com>
2024-05-06 23:08:51 +0600

    carousel and card section completed

---
+++ index.php
@@ -0,0 +1,288 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>U2 Exclusive</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="index.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"><img class="logo" height="30" width="30" src="img/logo.png" alt=""></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"></a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            Login
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="account.php">Account</a></li>
+                            <li><a class="dropdown-item" href="login.php">Login</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Bye Now</a></li>
+                        </ul>
+                    </li>
+
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+
+    <!--carousel-->
+    <div id="carouselExampleIndicators" class="carousel slide">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+                aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+                aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+                aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="banner" src="img/banner.jpg" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img class="banner" src="img/banner2.jpg" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img class="banner" src="img/banner3.jpg" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+    <!--coursel ends here--->
+
+
+    <!--card section starts here-->
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="card">
+                    <div class="card-img">
+                        <img class="c-img" src="img/c1.webp" alt="">
+                    </div>
+                    <div class="card-title">
+                        <h3> Name : T-shirt</h3>
+                    </div>
+                    <div class="card-current-price">
+                        <h4>price: 550/- <span class="text-warning"> <del>700</del></span></h4>
+                    </div>
+
+                    <div class="card-desc">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis obcaecati itaque facere a,
+                            
+                        </p>
+                    </div>
+                    <div class="add-to-cart">
+                        <button class="btn btn-success d-block">add to cart</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="card">
+                    <div class="card-img">
+                        <img class="c-img" src="img/c1.webp" alt="">
+                    </div>
+                    <div class="card-title">
+                        <h3> Name : T-shirt</h3>
+                    </div>
+                    <div class="card-current-price">
+                        <h4>price: 550/- <span class="text-warning"> <del>700</del></span></h4>
+                    </div>
+
+                    <div class="card-desc">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis obcaecati itaque facere a,
+                            
+                        </p>
+                    </div>
+                    <div class="add-to-cart">
+                        <button class="btn btn-success  d-block">add to cart</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="card">
+                    <div class="card-img">
+                        <img class="c-img" src="img/c1.webp" alt="">
+                    </div>
+                    <div class="card-title">
+                        <h3> Name : T-shirt</h3>
+                    </div>
+                    <div class="card-current-price">
+                        <h4>price: 550/- <span class="text-warning"> <del>700</del></span></h4>
+                    </div>
+
+                    <div class="card-desc">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis obcaecati itaque facere a,
+                            
+                        </p>
+                    </div>
+                    <div class="add-to-cart">
+                        <button class="btn btn-success  d-block">add to cart</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="card">
+                    <div class="card-img">
+                        <img class="c-img" src="img/c1.webp" alt="">
+                    </div>
+                    <div class="card-title">
+                        <h3> Name : T-shirt</h3>
+                    </div>
+                    <div class="card-current-price">
+                        <h4>price: 550/- <span class="text-warning"> <del>700</del></span></h4>
+                    </div>
+
+                    <div class="card-desc">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis obcaecati itaque facere a,
+                            
+                        </p>
+                    </div>
+                    <div class="add-to-cart">
+                        <button class="btn btn-success  d-block">add to cart</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3">
+                <div class="card">
+                    <div class="card-img">
+                        <img class="c-img" src="img/c1.webp" alt="">
+                    </div>
+                    <div class="card-title">
+                        <h3> Name : T-shirt</h3>
+                    </div>
+                    <div class="card-current-price">
+                        <h4>price: 550/- <span class="text-warning"> <del>700</del></span></h4>
+                    </div>
+
+                    <div class="card-desc">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis obcaecati itaque facere a,
+                            
+                        </p>
+                    </div>
+                    <div class="add-to-cart">
+                        <button class="btn btn-success d-block">add to cart</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="card">
+                    <div class="card-img">
+                        <img class="c-img" src="img/c1.webp" alt="">
+                    </div>
+                    <div class="card-title">
+                        <h3> Name : T-shirt</h3>
+                    </div>
+                    <div class="card-current-price">
+                        <h4>price: 550/- <span class="text-warning"> <del>700</del></span></h4>
+                    </div>
+
+                    <div class="card-desc">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis obcaecati itaque facere a,
+                            
+                        </p>
+                    </div>
+                    <div class="add-to-cart">
+                        <button class="btn btn-success  d-block">add to cart</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="card">
+                    <div class="card-img">
+                        <img class="c-img" src="img/c1.webp" alt="">
+                    </div>
+                    <div class="card-title">
+                        <h3> Name : T-shirt</h3>
+                    </div>
+                    <div class="card-current-price">
+                        <h4>price: 550/- <span class="text-warning"> <del>700</del></span></h4>
+                    </div>
+
+                    <div class="card-desc">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis obcaecati itaque facere a,
+                            
+                        </p>
+                    </div>
+                    <div class="add-to-cart">
+                        <button class="btn btn-success  d-block">add to cart</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="card">
+                    <div class="card-img">
+                        <img class="c-img" src="img/c1.webp" alt="">
+                    </div>
+                    <div class="card-title">
+                        <h3> Name : T-shirt</h3>
+                    </div>
+                    <div class="card-current-price">
+                        <h4>price: 550/- <span class="text-warning"> <del>700</del></span></h4>
+                    </div>
+                    <div class="card-desc">
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum quas assumenda beatae
+                           
+                        </p>
+                    </div>
+                    <div class="add-to-cart">
+                        <button class="btn btn-success  d-block">add to cart</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--card section end here--->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
