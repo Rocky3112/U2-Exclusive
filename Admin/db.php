e0e58d64a6fb790f9a90427f1337e19f4b252622
Rocky3112 <alomgirhossainrocky@gmail.com>
2024-05-25 23:39:23 +0600

    database successfully connected

--- Admin/db.php
+++ Admin/db.php
@@ -1,9 +1,9 @@
 <?php
 $con = mysqli_connect("localhost", "root", "", "ecommerce");
 
-if (!$con) {
-    echo("Connection failed: " . mysqli_connect_error());
-} else {
-    echo "Connected successfully";
-}
+// if (!$con) {
+//     echo("Connection failed: " . mysqli_connect_error());
+// } else {
+//     echo "Connected successfully"; connecteddd
+// }
 ?>
